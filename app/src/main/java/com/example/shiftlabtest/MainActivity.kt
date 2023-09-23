639177b491bcbf80d2f1d94253b1848adf7c3b47
Дмитрий Лейман <dimonlimon787@mail.ru>
2023-09-24 02:43:14 +0700

    textInputLayout realisation

--- app/src/main/java/com/example/shiftlabtest/MainActivity.kt
+++ app/src/main/java/com/example/shiftlabtest/MainActivity.kt
@@ -19,9 +19,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var registerButton: Button
 
     //creating variables for validation
-    private lateinit var userName: EditText
-    private lateinit var userSurname: EditText
-    private lateinit var userBirthday: EditText
+    private lateinit var userName: TextInputLayout
+    private lateinit var userSurname: TextInputLayout
+    private lateinit var userBirthday: TextInputLayout
     private lateinit var userPassword: TextInputLayout
     private lateinit var userConfirmedPassword: TextInputLayout
 
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         userName = findViewById(R.id.nameText)
         userSurname = findViewById(R.id.surnameText)
-        userBirthday = findViewById(R.id.birthText)
+        userBirthday = findViewById(R.id.birthdayText)
         userPassword = findViewById(R.id.passwordText)
         userConfirmedPassword = findViewById(R.id.passwordConfirmText)
 
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
                 myCalendar.set(Calendar.DAY_OF_MONTH, day)
                 updateLabel()
             }
-        userBirthday.setOnClickListener {
+        userBirthday.editText?.setOnClickListener {
             DatePickerDialog(this@MainActivity, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show()
         }
 
@@ -61,31 +61,31 @@ class MainActivity : AppCompatActivity() {
     private fun updateLabel() {
         val myFormat = "MM.dd.yyyy"
         val dateFormat = SimpleDateFormat(myFormat, Locale.US)
-        userBirthday.setText(dateFormat.format(myCalendar.time))
+        userBirthday.editText?.setText(dateFormat.format(myCalendar.time))
     }
 
     private fun validate(): Boolean {
-        var name: String = userName.text.toString()
-        var surname: String = userSurname.text.toString()
-        var birthday: String = userBirthday.text.toString()
-        var password: String = userPassword.toString()
-        var confirmedPassword: String = userConfirmedPassword.toString()
+        var name: String = userName.editText?.text.toString()
+        var surname: String = userSurname.editText?.text.toString()
+        var birthday: String = userBirthday.editText?.text.toString()
+        var password: String = userPassword.editText?.text.toString()
+        var confirmedPassword: String = userConfirmedPassword.editText?.text.toString()
 
 
         if (name.isEmpty() || name.length < 2) {
-            showHint(userName, "Имя должно содержать более 2 символов")
+            showHint(userName.editText, "Имя должно содержать более 2 символов")
             return false
         } else if (surname.isEmpty() || surname.length < 2) {
-            showHint(userSurname, "Фамилия должна содержать более 2 символов")
+            showHint(userSurname.editText, "Фамилия должна содержать более 2 символов")
             return false
         } else if (birthday.isEmpty()) {
-            showHint(userBirthday, "Введите значение")
+            showHint(userBirthday.editText, "Введите значение")
             return false
         } else if (password.isEmpty() || !password.contains(".")) {
-            showHint(userPassword, "Слабый пароль. Добавьте цифры и другие символы")
+            showHint(userPassword.editText, "Слабый пароль. Добавьте цифры и другие символы")
             return false
         } else if (confirmedPassword.isEmpty() || confirmedPassword != password) {
-            showHint(userConfirmedPassword, "Пароли не сходятся")
+            showHint(userConfirmedPassword.editText, "Пароли не сходятся")
             return false
         } else {
             return true
@@ -96,9 +96,6 @@ class MainActivity : AppCompatActivity() {
     private fun showHint(text: EditText?, hint: String) {
         text?.error = hint
         text?.requestFocus()
-    }
-    private fun showHint(text: TextInputLayout?, hint: String) {
-        text?.error = hint
     }
 }
 
